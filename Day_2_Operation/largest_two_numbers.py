00f8a7def809edee7de149ad6732adeab61fb5f9
nivethar023-git <nivethar023@gmail.com>
2025-11-21 20:06:53 +0530

    program to find the largest of two numbers
    What it does: It compares two numbers using a simple conditional check.
     concept :you take two inputs, and you use a conditional statement to check which one is bigger.

---
+++ Day_2_Operation/largest_two_numbers.py
@@ -0,0 +1,9 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+if a > b:
+    print(a, "is larger")
+elif b > a:
+    print(b, "is larger")
+else:
+    print("Both are equal")
