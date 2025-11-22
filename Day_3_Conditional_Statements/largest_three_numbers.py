7583d3585d90172b12f86b3d2dfc30e724892174
nivethar023-git <nivethar023@gmail.com>
2025-11-22 10:38:05 +0530

    program to find largest among three numbers using conditions
    Concept: Use sorting or comparisons to identify the top three highest values from a list of numbers.
    What it does: Takes user input, orders the numbers, and outputs the three largest in descending order.

---
+++ Day_3_Conditional_Statements/largest_three_numbers.py
@@ -0,0 +1,9 @@
+a=int(input("enter a number 1:"))
+b=int(input("enter a number 2:"))
+c=int(input("enter a number 3:"))
+if a>b and a>c:
+    print("A is largest",a)
+elif b>a and b>c:
+    print("B is largest",b)
+else:
+    print("c is largest",c)
